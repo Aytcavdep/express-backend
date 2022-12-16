ab37953d49c67eece9cb05a26f61e50e189b0a82
Valdemar <sazonov2380@gmail.com>
2022-12-16 12:22:07 +0000

    Update 15 files. Express create complete

--- index.js
+++ index.js
@@ -1,54 +1,85 @@
+import multer from "multer";
 //чтобы понимался Import, необходимо добавить в package.json "type": "module",
-import express from 'express';
-import { validationResult } from 'express-validator';
-import jwt from 'jsonwebtoken';
+import express from "express";
 //импорт для работы с mongodb
-import mongoose from 'mongoose';
-import { registerValidation } from './validations/auth.js';
+import mongoose from "mongoose";
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
+import { UserController, PostController } from "./controllers/index.js";
+import { checkAuth, handleValidationErrors } from "./utils/index.js";
+
 //коннект к базе mongodb
 mongoose
   .connect(
-    'mongodb+srv://Admin:7186@cluster0.v9pigjk.mongodb.net/?retryWrites=true&w=majority'
+    "mongodb+srv://Admin:7189@cluster0.v9pigjk.mongodb.net/blog?retryWrites=true&w=majority"
   )
-  .then(() => console.log('DB OK'))
-  .catch((err) => console.log('DB error', err));
+  .then(() => console.log("DB OK"))
+  .catch((err) => console.log("DB error", err));
 
 //апп это експресс сервер
 const app = express();
 //чтобы сервер понимал запросы json
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('1111 Hello world');
+//проверка на наличие статичных файло в директории
+app.use("/uploads", express.static("uploads"));
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+app.get("/", (req, res) => {
+  res.send("1111 Hello world");
 });
 
-app.post('/auth/register', registerValidation, (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
-  }
+app.post(
+  "/auth/register",
+  registerValidation,
+  handleValidationErrors,
+  UserController.register
+);
+app.post(
+  "/auth/login",
+  loginValidation,
+  handleValidationErrors,
+  UserController.login
+);
+app.get("/posts", PostController.getAll);
+app.get("/posts/:id", PostController.getOne);
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
   res.json({
-    succes: true,
+    url: `/uploads/${req.file.originalname}`,
   });
 });
-/*app.post('/auth/login', (req, res) => {
-  //создание токена для авторизации
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullName: 'Иванов Иван',
-    },
-    'password'
-  );
-  res.json({
-    success: true,
-    token,
-  });
-});*/
+app.get("/auth/me", checkAuth, UserController.getMe);
+app.post(
+  "/posts",
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.create
+);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch(
+  "/posts/:id",
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.update
+);
+
 //запуск сервера на порте 4444 и обработка ошибки запуска сервера
 app.listen(4444, (err) => {
   if (err) {
     return console.log(err);
   }
-  console.log('server OK');
+  console.log("server OK");
 });
---
+++ models/Post.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const PostShema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    tags: {
+      type: Array,
+      default: [],
+    },
+    imageUrl: String,
+    viewsCount: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+
+  { timestamps: true }
+);
+
+export default mongoose.model("Post", PostShema);
